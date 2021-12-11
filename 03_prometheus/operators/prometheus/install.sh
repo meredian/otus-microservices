b62c217b39fdfcc50fdea76271158a5fe55a58b7
Anton Sidelnikov <ndmeredian@gmail.com>
2021-12-11 16:43:54 +0700

    Save dashboards

--- 03_prometheus/operators/prometheus/install.sh
+++ 03_prometheus/operators/prometheus/install.sh
@@ -1,4 +1,8 @@
 #/usr/bin/env bash
+
+# We use relative script path, so we set proper directory first
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 # helm upgrade -n monitoring --version "21.0.0" -f ./prometheus-values.yaml prometheus prometheus-community/kube-prometheus-stack
 
 IS_INSTALLED=$(helm list -n monitoring | grep prometheus);
